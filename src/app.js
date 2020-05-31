8b1d94299fe037888c119fb8c738afd6386066b3
francosta <francisco@fcosta.pt>
2020-05-31 09:19:18 +0100

    Basic doc config.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ require('./models/Yon');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const swaggerUi = require('swagger-ui-express');
+const openApiDocumentation = require('../openApiDocumentation.json');
+
 const userRoutes = require('./routes/user');
 const yonRoutes = require('./routes/yon');
 
@@ -11,5 +14,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(userRoutes);
 app.use(yonRoutes);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
 
 module.exports = app;
